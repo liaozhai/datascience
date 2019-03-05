f7895f33fbf8f616978178685f3bed380f78ef60
liaozhai <marunov@scanex.ru>
2019-03-05 19:33:34 +0300

    Updated

--- social.ml
+++ social.ml
@@ -3,23 +3,17 @@ open Printf
 type user =
     {id: int; name: string; mutable friends: int list}
 
-let create_user id name =
-    {id; name; friends = []}
-
 let create_users names =
-    List.mapi create_user names
+    List.mapi (fun id name -> {id; name; friends = []}) names
     |> List.fold_left (fun a x -> x :: a) []
     |> List.rev
     |> Array.of_list
 
-let add_friends users links =
+let add_friends users =
     List.iter (fun (i, j) ->
         users.(i).friends <- j :: users.(i).friends;
         users.(j).friends <- i :: users.(j).friends
-    ) links
-
-let num_of_friends {friends} =
-    List.length friends
+    )
 
 let print_users users =
     for i = 0 to Array.length users - 1 do
@@ -33,16 +27,16 @@ let print_users users =
         ) friends
     done
 
-let users_stats users =
-    Array.map (fun {id; friends} -> id, List.length friends) users
+let users_stats =
+    Array.map (fun {id; friends} -> id, List.length friends)
 
 (** friend of a friend function stub *)
 let foaf users = ()
     
 let () =
     let users = create_users ["Hero"; "Dunn"; "Sue"; "Chi"; "Thor"; "Clive"; "Hicks"; "Devin"; "Kate"; "Klein"] in
-    [ 0,1; 0,2; 1,2; 1,3; 2,3; 3,4; 4,5; 5,6; 5,7; 6,8; 7,8; 8,9 ] |> add_friends users;
-    let total_links = Array.fold_left (fun a x -> a + num_of_friends x) 0 users |> float in
+    add_friends users [ 0,1; 0,2; 1,2; 1,3; 2,3; 3,4; 4,5; 5,6; 5,7; 6,8; 7,8; 8,9 ];
+    let total_links = Array.fold_left (fun a {friends} -> a + List.length friends) 0 users |> float in
     let total_users = Array.length users |> float in
     let avg_links = total_links /. total_users in    
     print_users users;
